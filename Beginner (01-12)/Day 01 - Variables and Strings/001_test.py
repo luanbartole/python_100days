b8542056208f855c20e920f7c9a0bc34152283d5
luan <luan.bartole@outlook.com>
2024-12-17 12:34:54 -0300

    hello-world

--- Beginner (01-12)/Day 01 - Variables and Strings/001_test.py
+++ Beginner (01-12)/Day 01 - Variables and Strings/001_test.py
@@ -1,1 +1,10 @@
-print("this is a test")
+print("-"*25)
+print("Space Explorer Creator")
+print("-"*25)
+astro = input("Astronaut's Name: ")
+spaceship = input("Spaceship's Name: ")
+planet = input("Planet: ")
+print("-"*25)
+print(f"Captain {astro}, you are commanding the spaceship {spaceship}. Your Mission: discover the secrets of the planet {planet}!")
+
+
