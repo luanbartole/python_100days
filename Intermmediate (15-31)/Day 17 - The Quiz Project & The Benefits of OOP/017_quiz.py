3e8dc41be98e2c6a564773d5012818f1104d61e9
luan <luan.bartole@outlook.com>
2025-04-01 12:22:35 -0300

    quiz

---
+++ Intermmediate (15-31)/Day 17 - The Quiz Project & The Benefits of OOP/017_quiz.py
@@ -0,0 +1,21 @@
+from question_model import Question
+from quiz_brain import Quizbrain
+import data
+
+question_bank = []
+
+for question in data.question_data:
+    new_question = Question(question['text'], question['answer'])
+    question_bank.append(new_question)
+
+quiz = Quizbrain(question_bank)
+while quiz.still_has_questions():
+    answer = quiz.next_question()
+
+print("="*25)
+print("End of the Quiz")
+print(f"Final Score: {quiz.score}/{quiz.question_number}")
+print("="*25)
+
+
+
---
+++ Intermmediate (15-31)/Day 17 - The Quiz Project & The Benefits of OOP/data.py
@@ -0,0 +1,14 @@
+new_question_data = [
+    {"text": "Bananas are berries, but strawberries are not.", "answer": "True"},
+    {"text": "Octopuses have three hearts.", "answer": "True"},
+    {"text": "The Great Wall of China is visible from space with the naked eye.", "answer": "False"},
+    {"text": "A group of flamingos is called a 'flamboyance'.", "answer": "True"},
+    {"text": "Water boils at the same temperature regardless of altitude.", "answer": "False"},
+    {"text": "The Eiffel Tower can grow taller in the summer due to heat expansion.", "answer": "True"},
+    {"text": "Sharks are mammals.", "answer": "False"},
+    {"text": "Honey never spoils.", "answer": "True"},
+    {"text": "An adult human has 300 bones.", "answer": "False"},
+    {"text": "A day on Venus is longer than a year on Venus.", "answer": "True"},
+    {"text": "The speed of sound is faster in water than in air.", "answer": "True"},
+    {"text": "Penguins can fly for short distances.", "answer": "False"}
+]
